f5e20e9a7d3f6f1d82b6675c7b9094545c1584dd
Perevyazko <a.perevyazko@gmail.com>
2022-11-20 20:04:33 +0300

    Зарегистрировал модели в админке

--- news/simpleapp/admin.py
+++ news/simpleapp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import News
+from .models import Author, Comment, News
 # from .. import simpleapp
 
 admin.site.register(News)
+admin.site.register(Author)
+admin.site.register(Comment)

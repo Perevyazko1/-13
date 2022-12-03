0c7e4810849e71826beef5ea341e24df5b9af0f4
Perevyazko <a.perevyazko@gmail.com>
2022-12-03 21:28:09 +0300

    Добавил возможность обнуления лайков в админке

--- news/simpleapp/admin.py
+++ news/simpleapp/admin.py
@@ -1,16 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 from django.utils.safestring import mark_safe
 
 from .models import Author, Comment, News, NewsCategory
 
 
 def nullfy_rating(modeladmin, request,
-                    queryset):  # request — объект хранящий информацию о запросе и queryset — грубо говоря набор
+                  queryset):  # request — объект хранящий информацию о запросе и queryset — грубо говоря набор
     # объектов, которых мы выделили галочками.
-    for object in queryset.values('rating'):
-        print(object)
+    for object in queryset.filter(rating__gt=0):
+        # print(object.id)
+        object.rating.clear()
+
 
 nullfy_rating.short_description = 'Обнулить лайки'  # описание для более понятного представления в админ панеле
+
+
 # задаётся, как будто это объект
 
 
@@ -24,10 +29,11 @@ class NewsAdmin(admin.ModelAdmin):
     list_display = ('dateCreation', 'title', 'author', 'total_likes', 'get_image')  # оставляем только имя и цену товара
     list_filter = ('dateCreation', 'author')  # добавляем примитивные фильтры в нашу админку
     search_fields = ('title', 'text')  # тут всё очень похоже на фильтры из запросов в базу
+    actions = [nullfy_rating]  # добавляем действия в список
 
+    def get_image(self, object):
+        return mark_safe(f'<img src = "{object.image.url}" width=50')
 
-    def get_image (self, object):
-        return mark_safe (f'<img src = "{object.image.url}" width=50')
 
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('dateCreation', 'commentPost', 'commentUser', 'text', 'total_likes_comment')
@@ -35,10 +41,10 @@ class CommentAdmin(admin.ModelAdmin):
     search_fields = ['text']
     actions = [nullfy_rating]  # добавляем действия в список
 
+
 admin.site.register(News, NewsAdmin)
 admin.site.register(Author, AuthorAdmin)
 admin.site.register(Comment, CommentAdmin)
 admin.site.register(NewsCategory)
-
 
 admin.site.site_title = 'Админ панель News Portal'

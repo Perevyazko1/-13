a3f43f08a293266353db1fafc4dc4759616c82f5
Perevyazko <a.perevyazko@gmail.com>
2022-12-03 19:33:23 +0300

    Подключил Postgres

--- news/news/settings.py
+++ news/news/settings.py
@@ -100,8 +100,11 @@ WSGI_APPLICATION = 'news.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- news/simpleapp/admin.py
+++ news/simpleapp/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import Author, Comment, News, NewsCategory
 
@@ -6,8 +7,8 @@ from .models import Author, Comment, News, NewsCategory
 def nullfy_rating(modeladmin, request,
                     queryset):  # request — объект хранящий информацию о запросе и queryset — грубо говоря набор
     # объектов, которых мы выделили галочками.
-    queryset.rating.delete()
-
+    for object in queryset.values('rating'):
+        print(object)
 
 nullfy_rating.short_description = 'Обнулить лайки'  # описание для более понятного представления в админ панеле
 # задаётся, как будто это объект
@@ -20,10 +21,13 @@ class AuthorAdmin(admin.ModelAdmin):
 # создаём новый класс для представления товаров в админке
 class NewsAdmin(admin.ModelAdmin):
     # list_display — это список или кортеж со всеми полями, которые вы хотите видеть в таблице с товарами
-    list_display = ('dateCreation', 'title', 'author', 'total_likes')  # оставляем только имя и цену товара
+    list_display = ('dateCreation', 'title', 'author', 'total_likes', 'get_image')  # оставляем только имя и цену товара
     list_filter = ('dateCreation', 'author')  # добавляем примитивные фильтры в нашу админку
     search_fields = ('title', 'text')  # тут всё очень похоже на фильтры из запросов в базу
 
+
+    def get_image (self, object):
+        return mark_safe (f'<img src = "{object.image.url}" width=50')
 
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('dateCreation', 'commentPost', 'commentUser', 'text', 'total_likes_comment')
@@ -35,3 +39,6 @@ admin.site.register(News, NewsAdmin)
 admin.site.register(Author, AuthorAdmin)
 admin.site.register(Comment, CommentAdmin)
 admin.site.register(NewsCategory)
+
+
+admin.site.site_title = 'Админ панель News Portal'
